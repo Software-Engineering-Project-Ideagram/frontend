f295a9432974fe380cc8b5992018df33f6eb870c
Zahra Amirinezhad <zahraamiri1381216@yahoo.com>
2023-04-13 01:41:35 +0430

    main page added

---
+++ ideagram/pages/MainPage/MainPage.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import classes from "./MainPage.module.scss";
+import { Idea } from "../../components";
+import { useState } from "react";
+
+const MainPage = () => {
+  const [seeAll, setSeeAll] = useState(false);
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.toolbar}>
+        <button>Arts</button>
+        <button>Design & Tech</button>
+        <button>Food</button>
+        <button>Games</button>
+        <button>Films</button>
+        <button>Music</button>
+        <button>Publishing</button>
+        <button>Science</button>
+      </div>
+
+      <div className={classes.body}>
+        <button
+          className={`${classes.seeAllIdeas} ${
+            seeAll && classes.disableButton
+          }`}
+          onClick={() => setSeeAll(true)}
+          disabled={seeAll}
+        >
+          See All
+        </button>
+
+        <div className={`${classes.ideas} ${seeAll && classes.scrollable}`}>
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+          <Idea />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
