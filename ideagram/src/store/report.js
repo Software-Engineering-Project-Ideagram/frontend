04dfa579593b31cbf13f9c22cb76bc1b8cdbf693
Zahra_Saeedi <zahrasaeediceui@gmail.com>
2023-06-01 01:04:39 +0330

    Create report.js
    add report

---
+++ ideagram/src/store/report.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reportsSlice = createSlice({
+  name: "report",
+  initialState: { reports: [], reportsNum: 0 },
+  reducers: {
+    addReport(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.reports.find(
+        (item) => item.title === newItem.title
+      );
+    },
+});
+
+export const reportsActions = reportsSlice.actions;
+
+export const reportsReducer = reportsSlice.reducer;
